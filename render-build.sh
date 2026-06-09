7de1faac79aab954321a00337c88e7933589e1a9
amertak <firefoxmcxd009@gmail.com>
2026-06-09 09:32:34 +0700

    update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Installing dependencies..."
+echo "Installing yt-dlp..."
 
 apt-get update
 
@@ -8,7 +8,7 @@ apt-get install -y ffmpeg python3 python3-pip
 
 pip3 install -U yt-dlp
 
-npm install
-
 echo "yt-dlp version:"
 yt-dlp --version
+
+npm install
