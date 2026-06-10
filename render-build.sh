7370883410b4ab76e80835407fe8cc01c273b85a
amertak <firefoxmcxd009@gmail.com>
2026-06-10 09:56:01 +0700

    update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-echo "Installing yt-dlp..."
-
-apt-get update
-
-apt-get install -y ffmpeg python3 python3-pip
-
-pip3 install -U yt-dlp
-
-echo "yt-dlp version:"
-yt-dlp --version
-
+# ដំឡើង dependencies
 npm install
+
+# បង្កើត folder bin
+mkdir -p bin
+
+# ទាញយក yt-dlp ជំនាន់ Linux
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o bin/yt-dlp
+chmod a+rx bin/yt-dlp
+
+# ទាញយក FFmpeg ជំនាន់ Linux Static
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
+tar -xf ffmpeg.tar.xz
+mv ffmpeg-*-amd64-static/ffmpeg bin/
+mv ffmpeg-*-amd64-static/ffprobe bin/
+chmod a+rx bin/ffmpeg bin/ffprobe
+
+# លុបកាកសំណល់
+rm -rf ffmpeg.tar.xz ffmpeg-*-amd64-static
